98b52c3b6ef047595838be2e9ecd8206b7b21d8e
Kaminishikawahara KK <kkaori146@gmail.com>
2021-09-18 15:17:36 +0900

    Building the API URL

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ document.querySelector('.busca').addEventListener('submit', (event)=>{
 
     if(input !== '') {
         showWarning('Loading...');
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=814f8c43d3c28d306f7d36c023b9bb91&units=metric&lang=en`;
     } 
 });
 
