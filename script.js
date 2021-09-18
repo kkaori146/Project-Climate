4d6c6e8b229b0b05aafc00f63574e5d0e8fe5fe4
Kaminishikawahara KK <kkaori146@gmail.com>
2021-09-18 15:21:24 +0900

    Request: Structure

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('.busca').addEventListener('submit', (event)=>{
+document.querySelector('.busca').addEventListener('submit', async(event)=>{
     event.preventDefault();
 
     let input = document.querySelector('#searchInput').value;
@@ -7,6 +7,12 @@ document.querySelector('.busca').addEventListener('submit', (event)=>{
         showWarning('Loading...');
 
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=814f8c43d3c28d306f7d36c023b9bb91&units=metric&lang=en`;
+
+        let results = await fetch(url);
+        let json = await results.json();   
+        
+        console.log(json);
+
     } 
 });
 
