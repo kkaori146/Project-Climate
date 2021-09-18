acb29189e3e84174199e2a6d6b486c3ba5514179
Kaminishikawahara KK <kkaori146@gmail.com>
2021-09-18 15:45:45 +0900

    TempIcon

--- script.js
+++ script.js
@@ -37,6 +37,8 @@ function showInfo(json) {
     document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>°C</sup>`;
 
     document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
+
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png` );
 }
 
 function showWarning(msg) {
